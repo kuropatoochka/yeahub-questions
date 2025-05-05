902db633b3f051c2bbeab8b3394e17b54a379e2b
kuropatoochka <valya000999@gmail.com>
2025-05-05 20:20:04 +0300

    fix bugs

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -8,9 +8,9 @@ import store from "./appStore.ts";
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-  <Provider store={store}>
-    <RouterProvider router={router}/>
-  </Provider>
-  // </StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
+  </StrictMode>
 )
